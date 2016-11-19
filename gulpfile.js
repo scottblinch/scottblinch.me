bef0a3881c19c4fe4a890b205897c824cadf9835
Scott Blinch <scott@tbkcreative.com>
2016-11-18 21:16:26 -0500

    gave in and did gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
+gulp.task('default', ['browser-sync', 'minify'], function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('css/**/*.css', ['minify']);
+    gulp.watch('**/*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    })
+});
+
+gulp.task('sass', function(){
+    return gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('minify', function() {
     return gulp.src([
             'css/normalize.min.css',
             'css/font-awesome.min.css',
@@ -18,5 +45,8 @@ gulp.task('default', function() {
             cascade: false
         }))
         .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
