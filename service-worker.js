a55bdad958af2893d813652d1c47e70bf2cd1e24
Scott Blinch <scott@tbkcreative.com>
2017-05-15 21:06:24 -0400

    forgot to add files

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'scottblinch.com-v1';
+var urlsToCache = [
+    '/',
+    '/img/dist/scott-blinch.jpg',
+    '/css/style.min.css',
+    '/js/main.min.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
